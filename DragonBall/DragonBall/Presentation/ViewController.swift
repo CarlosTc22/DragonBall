e827439b9c5d6346526a8b899ba24c572bf08a8b
Juan Carlos Torrejón Cañedo <carlostc22@gmail.com>
2024-02-14 10:14:59 +0100

    Add groups

---
+++ DragonBall/DragonBall/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DragonBall
+//
+//  Created by Juan Carlos Torrejon Cañedo on 14/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
